ea33054294de63a089b908394346bf80ffd11235
Nathan Perkins <nperkins487@gmail.com>
2016-03-21 00:32:56 +0000

    validations and messaging added to template

--- todo.rb
+++ todo.rb
@@ -15,16 +15,32 @@ get '/' do
   redirect '/lists'
 end
 
+# GET   /lists      -> view all lists
+# GET   /lists/new  -> new list form
+# POST  /lists      -> create new list
+# GET   /lists/1    -> view a single list
+
+# View list of lists
 get '/lists' do
   @lists = session[:lists]
   erb :lists, layout: :layout
 end
 
+
+# Render the new list form
 get '/lists/new' do
   erb :new_list, layout: :layout
 end
 
+# Create a new list
 post '/lists' do
-  session[:lists] << { name: params[:list_name], todos: [] }
-  redirect '/lists'
+  list_name = params[:list_name].strip
+  if (1..200).cover? list_name.size
+    session[:lists] << { name: list_name, todos: [] }
+    session[:success] = 'The list has been created.'
+    redirect '/lists'
+  else
+    session[:error] = 'List name must be between 1 and 200 characters long.'
+    erb :new_list, layout: :layout
+  end
 end
