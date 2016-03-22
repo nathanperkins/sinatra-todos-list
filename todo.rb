29b078dcf0a8a5f5d98574bced8a326b8a63ebf0
Nathan Perkins <nperkins487@gmail.com>
2016-03-22 00:57:49 +0000

    almost done

--- todo.rb
+++ todo.rb
@@ -53,8 +53,8 @@ post '/lists' do
 end
 
 # View the contents of one list
-get '/lists/:id' do
-  @list_id = params[:id].to_i
+get '/lists/:list_id' do
+  @list_id = params[:list_id].to_i
   @list = session[:lists][@list_id]
 
   erb :list, layout: :layout
@@ -105,18 +105,18 @@ post '/lists/:list_id/todos/:todo_id' do
 end
 
 # View the form for editing a list
-get '/lists/:id/edit' do
-  id = params[:id].to_i
-  @list = session[:lists][id]
+get '/lists/:list_id/edit' do
+  list_id = params[:list_id].to_i
+  @list = session[:lists][list_id]
 
   erb :edit_list, layout: :layout
 end
 
 # Update an existing todo list
-post '/lists/:id' do
+post '/lists/:list_id' do
   list_name = params[:list_name].strip
-  id = params[:id].to_i
-  @list = session[:lists][id]
+  list_id = params[:list_id].to_i
+  @list = session[:lists][list_id]
 
   error = error_for_list_name(list_name)
   if error
@@ -125,14 +125,27 @@ post '/lists/:id' do
   else
     @list[:name] = list_name
     session[:success] = 'The list name has been updated.'
-    redirect "/lists/#{id}"
+    redirect "/lists/#{list_id}"
+  end
+end
+
+# Mark all todos as completed
+post '/lists/:list_id/complete_all' do
+  if params[:complete_all] == 'true'
+    list_id = params[:list_id].to_i
+    list = session[:lists][list_id]
+    
+    list[:todos].each { |todo| todo[:completed] = true }
+    session[:success] = 'All todos were marked completed.'
+    
+    redirect "/lists/#{list_id}"
   end
 end
 
 # Delete the specified list.
-post '/lists/:id/destroy' do
-  id = params[:id].to_i
-  deleted_list = session[:lists].delete_at(id)
+post '/lists/:list_id/destroy' do
+  list_id = params[:list_id].to_i
+  deleted_list = session[:lists].delete_at(list_id)
   session[:success] = "The \"#{deleted_list[:name]}\" list has been deleted."
 
   redirect '/lists'
