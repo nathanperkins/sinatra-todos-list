bcd8c532443c3a0aca61af18ca8dc76a0e90bd88
Nathan Perkins <nperkins487@gmail.com>
2016-03-23 02:32:14 +0000

    added alert box and XHR

---
+++ public/javascripts/application.js
@@ -0,0 +1,28 @@
+$(function(){
+
+  $('form.delete').submit(function(event){
+    event.preventDefault();
+    event.stopPropagation();
+
+    var ok = confirm('Are you sure? This cannot be undone!')
+
+    if (ok) {
+      // this.submit();
+
+      var form = $(this)
+
+      var request = $.ajax({
+        url: form.attr('action'),
+        method: form.attr('method')
+      });
+
+      request.done(function(data, textStatus, jqHXR){
+        form.parent('li').remove();
+      });
+
+      request.fail(function(){
+
+      });
+    }
+  });
+});
